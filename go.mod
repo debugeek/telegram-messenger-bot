fe3f6e5c7fdbfef29d63fa1da093a9658bca580b
Xiao Jin <debugeek@gmail.com>
2024-11-15 09:48:46 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/debugeek/telegram-messenger-bot
+
+go 1.22.2
---
+++ msgr/msgr.go
@@ -0,0 +1,55 @@
+package msgr
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Payload struct {
+	ChatID    string `json:"chat_id"`
+	Text      string `json:"text"`
+	ParseMode string `json:"parse_mode"`
+}
+
+func Send(botToken, chatID, message string) error {
+	apiURL := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
+
+	payload := Payload{
+		ChatID:    chatID,
+		Text:      message,
+		ParseMode: "HTML",
+	}
+
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("message failed to send: %s", body)
+	}
+
+	return nil
+}
