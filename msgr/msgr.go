c1fec7e617f9c12c4e4f55e65b59bd634f4222dd
Xiao Jin <debugeek@gmail.com>
2025-01-08 20:50:01 +0800

    Commit

--- msgr/msgr.go
+++ msgr/msgr.go
@@ -8,31 +8,69 @@ import (
 	"net/http"
 )
 
-type Payload struct {
-	ChatID    string `json:"chat_id"`
+type Message struct {
+	ChatID           string `json:"chat_id"`
+	ReplyToMessageID string `json:"reply_to_message_id"`
+}
+
+type TextMessage struct {
+	Message
 	Text      string `json:"text"`
 	ParseMode string `json:"parse_mode"`
+}
+
+type VideoMessage struct {
+	Message
+	VideoID string `json:"video"`
 }
 
 type TgMsgr struct {
 	BotToken string
 }
 
-func (msgr *TgMsgr) Send(chatID, message string) error {
+func (msgr *TgMsgr) SendTextMessage(message TextMessage) error {
 	apiURL := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", msgr.BotToken)
 
-	payload := Payload{
-		ChatID:    chatID,
-		Text:      message,
-		ParseMode: "HTML",
-	}
-
-	payloadBytes, err := json.Marshal(payload)
+	jsonBytes, err := json.Marshal(message)
 	if err != nil {
 		return err
 	}
 
-	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(payloadBytes))
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("message failed to send: %s", body)
+	}
+
+	return nil
+}
+
+func (msgr *TgMsgr) SendVideoMessage(message VideoMessage) error {
+	apiURL := fmt.Sprintf("https://api.telegram.org/bot%s/sendVideo", msgr.BotToken)
+
+	jsonBytes, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(jsonBytes))
 	if err != nil {
 		return err
 	}
